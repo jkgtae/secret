1e8c465778cabe4ab00610c9ef283d0935960903
Jenny Kelly <jennykelly23@gmail.com>
2016-02-09 19:49:11 -0500

    Return empty list when there are no primes less than integer input

--- secret.py
+++ secret.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python
+
+
+def generate_primes(n):
+
+    # there are no primes less than 2
+    if n <= 2:
+        return []
--- test_secret.py
+++ test_secret.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import unittest
+from secret import *
 
 
 class GeneratePrimesTestCase(unittest.TestCase):
