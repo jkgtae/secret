f6a6da2fdadf6e55ebc39ab7180c72e7b4746143
Jenny Kelly <jennykelly23@gmail.com>
2016-02-09 20:52:41 -0500

    Print error message for non-integer input

--- secret.py
+++ secret.py
@@ -53,17 +53,22 @@ def is_secret_additive(secret=secret_additive):
 
     user_input = raw_input('Please input a number: ')
 
-    n = int(user_input)
+    # check that input is a number
+    try:
+        n = int(user_input)
+    except ValueError:
+        print 'You must input a number\n'
+    else:
 
-    # generate list of prime numbers less than n
-    primes = generate_primes(n)
+        # generate list of prime numbers less than n
+        primes = generate_primes(n)
 
-    # determine whether secret is additive for all combinations of primes less than n
-    for x, y in combinations(primes, 2):
-        print 'Checking primes %s and %s' % (x, y)
-        if secret(x+y) != secret(x) + secret(y):
-            print 'Secret is not additive\n'
-            return False
+        # determine whether secret is additive for all combinations of primes less than n
+        for x, y in combinations(primes, 2):
+            print 'Checking primes %s and %s' % (x, y)
+            if secret(x+y) != secret(x) + secret(y):
+                print 'Secret is not additive\n'
+                return False
 
-    print 'Secret is additive\n'
-    return True
+        print 'Secret is additive\n'
+        return True
